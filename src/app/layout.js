0f9a3fa6a8b489e65907065973ba18f143c18d3f
TomasNieto19 <tnieto852@gmail.com>
2024-01-10 00:55:21 -0300

    Vercel config

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,9 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import Container from "@mui/material/Container";
 import "./globals.css";
 
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+
 export const metadata = {
   title: "Tomás Nieto",
   description: "Portfolio of Tomás Nieto",
@@ -16,6 +19,8 @@ export default function RootLayout({ children }) {
           <Container maxWidth="md">
             <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
           </Container>
+          <Analytics />
+          <SpeedInsights />
         </main>
       </body>
     </html>
